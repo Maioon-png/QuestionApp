eac80514cb7ca9dc730204b04e096704f4ce4ab8
Maioon-png <mi_mkb.12_5@icloud.com>
2020-09-10 10:31:35 +0900

    add variables and methods

--- QuestionApp/Controller/NextViewController.swift
+++ QuestionApp/Controller/NextViewController.swift
@@ -8,23 +8,53 @@
 
 import UIKit
 
+protocol NowScoreDelegate {
+    
+    func nowScore(score: Int)
+    
+}
+
 class NextViewController: UIViewController {
 
+    @IBOutlet weak var correctLabel: UILabel!
+    @IBOutlet weak var wrongLabel: UILabel!
+    
+    var delegate:NowScoreDelegate?
+    
+    //画面遷移時に渡された値を受け取るための変数
+    var correctedCount = Int()
+    var wrongCount = Int()
+    
+    var beforeCount = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        correctLabel.text = String(correctedCount)
+        wrongLabel.text = String(wrongCount)
+        
+        //beforeCount(キー値)で保存されている値をbeforeCountの変数の中に入れる
+        if UserDefaults.standard.object(forKey: "beforeCount") != nil{
+            beforeCount = UserDefaults.standard.object(forKey: "beforeCount") as! Int
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //家ボタン押したら画面が戻る
+    @IBAction func back(_ sender: Any) {
+        //最高得点であれば入れ替え
+        if beforeCount < correctedCount{
+            //値を保存
+            UserDefaults.standard.set(correctedCount, forKey: "beforeCount")
+            //上で作ったデリゲートメソッドを呼び出す
+            delegate?.nowScore(score: correctedCount)
+        }else if beforeCount > correctedCount{ //そうでなければそのまま
+            UserDefaults.standard.set(beforeCount, forKey: "beforeCount")
+        }
+        
+        
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
 
 }
--- QuestionApp/Controller/ViewController.swift
+++ QuestionApp/Controller/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,NowScoreDelegate {
+
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -24,8 +25,22 @@ class ViewController: UIViewController {
     //検知した正答がどちらなのかを検知する変数
     var pickedAnswer = false
     
+    var soundFile = SoundFile()
+    
+    var changeColor = ChangeColor()
+    
+    var gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gradientLayer = changeColor.changeColor(topR: 0.07, topG: 0.13, topB: 0.26, topAlpha: 1.0 ,bottomR: 0.54, bottomG: 0.74, bottomB: 0.74, bottomAlpha: 1.0)
+        //画面全体に広がってくれ
+        gradientLayer.frame = view.bounds
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        imageView.layer.cornerRadius = 20.0
         
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -36,11 +51,20 @@ class ViewController: UIViewController {
         questionNumber = 0
         
         imageView.image = UIImage(named: imagesList.list[0].imageText)
+        
+        //最高得点のラベルに値を入れる
+        if UserDefaults.standard.object(forKey: "beforeCount") != nil{
+            maxScore = UserDefaults.standard.object(forKey: "beforeCount") as! Int
+        }
+        maxScoreLabel.text = String(maxScore)
+        
     }
 
     
     @IBAction func answer(_ sender: Any) {
         if (sender as AnyObject).tag == 1 {
+            
+            soundFile.playSound(filename: "maruSound", extensionName: "mp3")
             
             pickedAnswer = true
             
@@ -49,6 +73,8 @@ class ViewController: UIViewController {
             //まるボタンの音声を流す
             
         }else if (sender as AnyObject).tag == 2 {
+            
+            soundFile.playSound(filename: "batsuSound", extensionName: "mp3")
             
             pickedAnswer = false
             
@@ -85,9 +111,28 @@ class ViewController: UIViewController {
         }else{
             print("問題終了")
             //画面遷移
-            performSegue(withIdentifier: <#T##String#>, sender: <#T##Any?#>)
+            performSegue(withIdentifier: "next", sender: nil)
         }
     }
+    
+    //今のスコアを反映
+    func nowScore(score: Int) {
+        soundFile.playSound(filename: "sound", extensionName: "mp3")
+        maxScoreLabel.text = String(score)
+    }
+    
+    //画面遷移時に値を渡す
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "next"{
+            let nextVC = segue.destination as! NextViewController
+            nextVC.correctedCount = correctCount
+            nextVC.wrongCount = wrongCount
+            nextVC.delegate = self
+        }
+        
+    }
+    
     
 }
 
